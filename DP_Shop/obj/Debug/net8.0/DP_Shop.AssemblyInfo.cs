9e414e3548eb71701e519b218e13633ab3b9d65c
thanhdat1047 <thanhdat1047t@gmail.com>
2024-12-13 10:41:10 +0700

    add file seed

--- DP_Shop/obj/Debug/net8.0/DP_Shop.AssemblyInfo.cs
+++ DP_Shop/obj/Debug/net8.0/DP_Shop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DP_Shop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b3a418c183962d84dfda565ba3fa2a6d5d6f1e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9dfcb300d21c4413232fdaa5effbf4d7da484f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DP_Shop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DP_Shop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
