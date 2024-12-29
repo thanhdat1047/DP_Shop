8699b3dc7dbebc69e75bcf2a67916c533ede36f8
thanhdat1047 <thanhdat1047t@gmail.com>
2024-12-29 17:24:18 +0700

    update user profile api

--- DP_Shop/DTOs/Users/UpdateUserRequestDto.cs
+++ DP_Shop/DTOs/Users/UpdateUserRequestDto.cs
@@ -12,6 +12,6 @@ namespace DP_Shop.DTOs.Users
         public string Email { get; set; } = string.Empty;
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be exactly 10 digits.")]
         public string PhoneNumber { get; set; } = string.Empty;
-        public List<string>? Roles { get; set; }
+        //public List<string>? Roles { get; set; }
     }
 }
